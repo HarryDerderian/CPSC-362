524c3ac16ec0ac0fd07ae204d347ac70af701c59
HarryDerderian <hderderian@student.cccd.edu>
2023-10-23 22:49:11 -0700

    update

--- Labs/Lab_4/LabSession0401.py
+++ Labs/Lab_4/LabSession0401.py
@@ -14,7 +14,6 @@ def init_db() :
     data_base.commit()
     data_base.close()
 
-
 def verify_input() -> bool :
     """Confirms all user input is filled in and correct"""
     name = name_user_input.get()
@@ -35,6 +34,35 @@ def verify_input() -> bool :
         return False
     else : 
         return True
+
+def search_product() -> None :
+    """Search the database for product based on user input"""
+    # Confirm user input, some options are allowed empty.
+    name = name_user_input.get()
+    manufacturer = manufacturer_input.get()
+    expiry = expiry_user_input.get()
+    stock = stock_user_input.get()
+    if not name and not manufacturer and not expiry and not stock :
+        messagebox.showerror("Error", "Please fill in at least one of the required fields.")
+        return  
+    # Confirm that stock count is an int > -1, or ignore it if left blank.
+    try : # make sure to catch invalid int conversions
+        if stock and int(stock) < 0 :
+            messagebox.showerror("Error", "Stock count can't be negative.")
+            return
+    except :
+        messagebox.showerror("Error", "Stock must be an int")
+        return
+    # Verification complete...
+    # Establish connection to database
+    data_base = sqlite3.connect('inventory.db')
+    data_control = data_base.cursor()
+    SQL_COMMAND = "SELECT * FROM products WHERE name = ? OR manufacturer = ? OR expiry = ? OR quantity = ?"
+    if stock : stock = int(stock)
+    data_table = data_control.execute(SQL_COMMAND, (name, manufacturer, expiry, stock))
+    view_products(data_table)
+    data_base.close()
+
 
 def del_product() -> None : 
     """Deleting a product from the database."""
@@ -58,7 +86,6 @@ def del_product() -> None :
     # Handle any unforseen runtime errors...
     except Exception as e: messagebox.showerror("Error", "Unable to find product with those fields.")
 
-
 def add_product() -> None :
     """Add a new product to the database."""
     # Confirm user input is correct.
@@ -81,23 +108,26 @@ def add_product() -> None :
         messagebox.showinfo("info", "Product added to inventory")
     except Exception as e: messagebox.showerror("Error", str(e))
 
-
-def view_products() -> None :
-    """Load and display all notes from the database."""
+def view_all() -> None :
     data_base = sqlite3.connect('inventory.db')
-    data_editor = data_base.cursor()
-    for row in data_editor.execute("SELECT * FROM products"):
+    data_control = data_base.cursor()
+    SQL_COMMAND = "SELECT * FROM products"
+    view_products(data_control.execute(SQL_COMMAND))
+    data_base.close()
+
+def view_products(data_table) -> None :
+    """Load and display all notes from the database.
+        @param data_table: a selected table of data from a database cursor
+    """
+    tree_view.delete(*tree_view.get_children())
+    for row in data_table : # data_editor.execute("SELECT * FROM products"):
         id = row[0]
         name = row[1]
         manufacturer = row[2]
         description = row[3]
         stock = row[4]
         expiry = row[5]
-        print("="*100)
-        print("ID: {}\nName: {}\nManufact: {}\nDescription: {}\nStock: {}\nExpiry: {}\n\n\n".format(id, name, manufacturer, description, stock, expiry))
-        #tree_view.insert(tk.END, row[0])
-    data_base.close()
-
+        tree_view.insert("", tk.END, values=(id, name, manufacturer, expiry, stock))
 
 
 # Initialize main window
@@ -111,6 +141,13 @@ root.resizable = False
 
 # Text labels
 LABEL_FONT = font.Font(family="Times new roman", size=15)
+
+
+# 'Item count' label :
+
+
+
+
 # 'Product name' label :
 name_label = tk.Label(root, text = "Product Name")
 name_label.place( x = 100, y = 50)
@@ -131,7 +168,6 @@ stock_label = tk.Label(root, text = "Stock")
 stock_label.place( x = 125, y = 320)
 stock_label['font'] = LABEL_FONT
 
-
 # Input text fields:
 TEXT_INPUT_WIDTH = 30 # (pixels)
 # Entry font updates the height of the Entry object
@@ -143,14 +179,12 @@ name_input = tk.Entry(root, width = TEXT_INPUT_WIDTH,
 name_input.place(x = 450, y = 45 )
 name_input['font'] = LABEL_FONT 
 
-
 # Manufacturer input bar
 manufacturer_user_input = tk.StringVar() # Gives access to the input within the associated text field.
 manufacturer_input = tk.Entry(root, width = TEXT_INPUT_WIDTH, 
                               textvariable = manufacturer_user_input)
 manufacturer_input.place(x = 450, y = 135 )
 manufacturer_input['font'] = LABEL_FONT
-
 
 # Expiry input bar
 expiry_user_input = tk.StringVar() # Gives access to the input within the associated text field.
@@ -167,7 +201,6 @@ stock_input = tk.Entry(root, width = TEXT_INPUT_WIDTH,
 stock_input.place(x = 450, y = 315 )
 stock_input['font'] = LABEL_FONT
 
-
 # Buttons :
 BUTTON_FONT = font.Font(family="Times new roman", size = 20)
 BUTTON_HEIGHT = 1 # (pixels)
@@ -182,12 +215,19 @@ add_button.place(x = 100, y = 370)
 del_button = tk.Button(root, text = "Delete Product",
                     width = BUTTON_WIDTH, height = BUTTON_HEIGHT,
                                                     command = del_product)
-del_button.place(x = 545, y = 370)
+del_button.place(x = 300, y = 370)
+
+# 'Search' button
+search_button = tk.Button(root, text = "Search Product",
+                    width = BUTTON_WIDTH, height = BUTTON_HEIGHT,
+                                                    command = search_product)
+search_button.place(x = 800, y = 370)
+
 
 # 'View' button
 view_button = tk.Button(root, text = "View Products",
                     width = BUTTON_WIDTH, height = BUTTON_HEIGHT,
-                                                    command = view_products)
+                                                    command = view_all)
 view_button.place(x = 1000, y = 370)
 
 # Tree view (main data display) 
@@ -216,16 +256,6 @@ tree_view.column("#5", width=240)
 # set cords
 tree_view.place( x= 0, y = 410)
 
-# Define StringVar for storing notes
-note_var = tk.StringVar()
-# Create entry widget and buttons
-#tk.Entry(root, textvariable=note_var, width=50).pack(padx=10, pady=10)
-#tk.Button(root, text="Add Note", command=add_note).pack(padx=10, pady=5)
-#tk.Button(root, text="Delete Note", command=delete_note).pack(padx=10, pady=5)
-# Create a listbox to display notes
-#listbox = tk.Listbox(root, width=50)
-#listbox.pack(padx=10, pady=10)
-#init_db() # Initialize the database and create table if not exists
-#load_notes() # Load notes on startup
+
 init_db()
 root.mainloop()
